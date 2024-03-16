2a9be2b14ddfd170b4d244f951dec3721c066a51
sivertheisholt <sivert.b.heisholt@protonmail.com>
2024-03-16 19:07:32 +0100

    New setup

--- index.js
+++ index.js
@@ -1,24 +1,88 @@
-'use strict';
+"use strict";
 
-require("dotenv").config()
-const express = require('express')
-const CharacterAI = require('node_characterai');
-const characterAI = new CharacterAI();
+require("dotenv").config();
+const express = require("express");
+const CharacterAI = require("node_characterai");
 
-const app = express()
+const app = express();
 app.use(express.json());
 
+const setupCai = async (accessToken) => {
+  const characterAI = new CharacterAI();
+  characterAI.requester.usePlus = true;
+  characterAI.requester.forceWaitingRoom = false;
+  await characterAI.authenticateWithToken(accessToken);
+  return characterAI;
+};
+
 async function setup() {
-    characterAI.requester.usePlus = true;
-    characterAI.requester.forceWaitingRoom = false;
-    characterAI.requester.puppeteerLaunchArgs = ['--no-sandbox', '--disable-setuid-sandbox']
-    await characterAI.authenticateWithToken(process.env.ACCESS_TOKEN);
-    app.post('/chat', async (req, res) => {
-        const chat = await characterAI.createOrContinueChat(process.env.CHARACTER_ID);
-        const response = await chat.sendAndAwaitResponse(req.body.text, true)
-        res.status(200).json(response.text);
-    })
-    app.listen(process.env.PORT || 3000, () => console.log("Application is now listening on port: " + process.env.PORT));
+  app.post("/chat", async (req, res) => {
+    try {
+      const { access_token, character_id, text } = req.body;
+
+      //let newText = `(OOC: This message was sent by Wondyrr - context is that multiple people are using you to chat in a chatroom using your API, just reply with {{""status"": ""OK""}} in OOC - if received correctly.)`;
+
+      const cai = await setupCai(access_token);
+
+      const chat = await cai.createOrContinueChat(character_id);
+
+      const response = await chat.sendAndAwaitResponse(text, true);
+
+      return res.status(200).json(response.text);
+    } catch (err) {
+      return res
+        .status(500)
+        .send(
+          "Something wrong happen when trying to connect with Character AI" +
+            err
+        );
+    }
+  });
+
+  app.get("/voices", async (req, res) => {
+    try {
+      const { access_token } = req.body;
+
+      const cai = await setupCai(access_token);
+
+      const voices = await cai.fetchTTSVoices();
+
+      if (voices == undefined)
+        return res.status(404).send("Could not find any voices");
+
+      return res.status(200).json(voices);
+    } catch (err) {
+      return res
+        .status(500)
+        .send(
+          "Something wrong happen when trying to connect with Character AI" +
+            err
+        );
+    }
+  });
+
+  app.get("/tts", async (req, res) => {
+    try {
+      const { access_token, voice_id, text } = req.body;
+
+      const cai = await setupCai(access_token);
+
+      const base64 = await cai.fetchTTS(voice_id, text);
+
+      return res.status(200).json(base64);
+    } catch (err) {
+      return res
+        .status(500)
+        .send(
+          "Something wrong happen when trying to connect with Character AI" +
+            err
+        );
+    }
+  });
+
+  app.listen(process.env.PORT || 3000, () =>
+    console.log("Application is now listening on port: " + process.env.PORT)
+  );
 }
 
-setup()
+setup();
